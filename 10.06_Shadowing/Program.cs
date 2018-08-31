9cc9a505ce7efcda5a781bc363097c025382df92
Lubor Martinský <lubor.martinsky@hotmail.com>
2018-08-31 11:38:37 +0200

    uncomment

--- 10.06_Shadowing/Program.cs
+++ 10.06_Shadowing/Program.cs
@@ -11,5 +11,39 @@ namespace _10._06_Shadowing
         static void Main(string[] args)
         {
         }
+
+
+        class Animal
+        {
+            //public virtual string makeSound()
+            public string makeSound()
+            {
+                return "";
+            }
+        }
+
+        class Fish : Animal
+        {
+
+        }
+
+        class Cat : Animal
+        {
+            //public override string makeSound()
+            new public string makeSound()
+            {
+                return "Mnau";
+            }
+        }
+
+        //class PersianCat : Cat
+        //{
+        //    public override string makeSound()
+        //    {
+        //        return "Mnau Mnau";
+        //    }
+        //}
+
     }
 }
+
